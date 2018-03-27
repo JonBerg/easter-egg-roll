453814457b07cacdcd8f354d0787ca83881c27e9
JonBerg <janis.berkis@wunder.io>
2018-03-27 13:55:44 +0300

    improvments

--- easter-roll.js
+++ easter-roll.js
@@ -41,6 +41,11 @@ function show_message(discount) {
           $( ".discount-code" ).text(code3);
           setCookie("code", code3);
           break;
+      case 4:
+          $( ".discount-text" ).text('Tu esi aizripinājis olu 20m tālu un laimējis 20% atlaidi žurnālu abonēšanai');
+          $( ".discount-code" ).text(code4);
+          setCookie("code", code4);
+          break;
   }
   var code = getCookie("code");
   $( ".try-agen-later" ).on( "click", function() {
@@ -50,7 +55,7 @@ function show_message(discount) {
 }
 var code = getCookie("code");
 //coockies
-var countdown = 100;
+var countdown = 1;
 //current timestamp
 var now   = Date.parse(new Date());
 //ready should be stored in your cookie
@@ -64,7 +69,7 @@ var message=getCookie("Time");
 var now  = Date.parse(new Date());
 if (now >= timeout) {
   $( ".roll-egg" ).one('click', function(e) {
-    var random_distace = Math.floor((Math.random() * 3) + 1);
+    var random_distace = Math.floor((Math.random() * 4) + 1);
     $( "div.egg" ).addClass('roll-'+random_distace+'m');
     setTimeout(function(){ show_message(random_distace)}, 3000);
     setCookie("ready", ready);
@@ -82,3 +87,19 @@ $( ".messageclose" ).on( "click", function() {
   $(".message-overlay").removeClass("show");
   $( ".messages h2" ).text('Ienāc rīt');
 });
+function copy(element_id){
+  var aux = document.createElement("div");
+  aux.setAttribute("contentEditable", true);
+  aux.innerHTML = document.getElementById(element_id).innerHTML;
+  aux.setAttribute("onfocus", "document.execCommand('selectAll',false,null)");
+  document.body.appendChild(aux);
+  aux.focus();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+}
+var encrypted = CryptoJS.AES.encrypt("Message", "Secret Passphrase");
+//U2FsdGVkX18ZUVvShFSES21qHsQEqZXMxQ9zgHy+bu0=
+
+var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+console.log(encrypted);
+console.log(decrypted);
